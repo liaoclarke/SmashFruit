388239ac3c15e62285e7aafc7cbba78a0009c5d4
liaoclark <liaoclarke@gmail.com>
2016-02-28 13:26:08 +0800

    Add new source code

---
+++ desktop/src/com/freemotion/smashfruit/android/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.freemotion.smashfruit.android.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.freemotion.smashfruit.android.SmashFruit;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SmashFruit(), config);
+	}
+}
